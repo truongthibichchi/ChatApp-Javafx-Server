c7482a8ad2bedff8913a8b6323675902c42f205c
Khánh <truongthibichchi2509@gmail.com>
2017-12-29 10:06:19 +0700

    ok before hibernate

--- Server/src/controllers/ServerWindowController.java
+++ Server/src/controllers/ServerWindowController.java
@@ -5,16 +5,15 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.text.TextAlignment;
+import org.apache.commons.lang3.text.WordUtils;
 
 public class ServerWindowController {
     @FXML private ListView lstLog;
 
     public void log (String msg) {
         Platform.runLater(() ->{
-            Label label= new Label(msg);
-            label.setWrapText(true);
-            label.setTextAlignment(TextAlignment.LEFT);
-            lstLog.getItems().add(msg);
+            lstLog.getItems().add(WordUtils.wrap(msg, 45));
+            lstLog.scrollTo(lstLog.getItems().size() - 1);
         });
     }
 }
